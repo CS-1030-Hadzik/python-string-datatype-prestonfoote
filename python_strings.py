6a7be3b70c1bebf62230ecd1ad6ae4b942b3f16a
Preston Foote <prestonfoote#@mail.weber.edu>
2020-01-15 09:18:24 -0700

    completed up to formatting strings

--- python_strings.py
+++ python_strings.py
@@ -8,8 +8,10 @@
 #       -set this equal to your birth year (doesn't have to be real should be less then 100 yrs ago)
 #   - current_year
 #       -set this equal to 2020
-
-
+my_first_name = 'Preston'
+my_last_name = 'Foote'
+my_year_of_birth = 2001
+current_year = 2020
 
 
 # TODO String Indexing
@@ -17,18 +19,21 @@
 #       - first name  
 #       - last name
 #       - first letter of your first name (use the +index)
-#       - second letter of your last name (use the -index)
+#       - last letter of your last name (use the -index)
 #       - first two letter of your first name (use the +index)
-#       - second two letter of your last name (use the -index)
-
-
-
-
+#       - last two letter of your last name (use the -index)
+print (my_first_name)
+print (my_last_name)
+print (my_first_name[0])
+print (my_last_name[-1])
+print (my_first_name[0:2])
+print (my_last_name[-2:])
 #TODO Combining Strings
 #   - Print the following items (one per line) (print using variables)
 #       -first name and last name combined
 #       -first name six times
-
+print (my_first_name + my_last_name)
+print (my_first_name * 6)
 
 
 
@@ -37,7 +42,7 @@
 #   - Print the following items (one per line) (print using variables)
 #       - first name last name -was born in- year of birth
 #       - first name last name -was born in- year of birth. first name -enjoyed celebrating- current year
-
+txt = my_first_name + my_last_name + 
 
 
 # TODO Escape characters
