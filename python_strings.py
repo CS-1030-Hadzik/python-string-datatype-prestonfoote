ade001df114b326f223438057779c9f0a7ee4240
Preston Foote <prestonfoote#@mail.weber.edu>
2020-01-22 08:58:07 -0700

    strings

--- python_strings.py
+++ python_strings.py
@@ -42,7 +42,7 @@ print (my_first_name * 6)
 #   - Print the following items (one per line) (print using variables)
 #       - first name last name -was born in- year of birth
 #       - first name last name -was born in- year of birth. first name -enjoyed celebrating- current year
-txt = my_first_name + my_last_name + 
+
 
 
 # TODO Escape characters
